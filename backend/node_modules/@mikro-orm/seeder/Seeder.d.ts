46c71544eff223b19763a65e5ef05bc6b1c17795
vincentiusraynaldi <94402344+vincentiusraynaldi@users.noreply.github.com>
2024-04-21 23:31:28 +0200

    Delete backend/node_modules directory

--- backend/node_modules/@mikro-orm/seeder/Seeder.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Dictionary, EntityManager } from '@mikro-orm/core';
-export declare abstract class Seeder<T extends Dictionary = Dictionary> {
-    abstract run(em: EntityManager, context?: T): void | Promise<void>;
-    protected call(em: EntityManager, seeders: {
-        new (): Seeder;
-    }[], context?: T): Promise<void>;
-}
